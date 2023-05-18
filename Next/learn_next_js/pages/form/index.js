c035142f335daff304683bdf0b26a66025324f21
unknown <21114290027@academico.iesb.br>
2023-05-17 21:56:55 -0300

    next Form

--- Next/learn_next_js/pages/form/index.js
+++ Next/learn_next_js/pages/form/index.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
 import BasePage from '../../components/BasePage'
-import { useForm } from "react-hook-form"
+import { Table } from 'react-bootstrap';
+import Link from 'next/link';
 
 const index = () => {
 
-    const { register, handleSubmit } = useForm()
+    const [data, setData] = useState([])
 
-    function salvar(dados){
-        console.log(dados)
-    }
+    useEffect(() => {
+        // const datalocal = JSON.parse(localStorage.getItem('cursos'));
+        setData(JSON.parse(localStorage.getItem('cursos')))
+    }, [])
+
+    console.log(data)
 
     return (
         <BasePage>
-            <Form>
-
-                <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="user name" {...register('name')}/>
-                </Form.Group>
-            
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" {...register('password')}/>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="modalidade">
-                    <Form.Label>Modalidade</Form.Label>
-                    <Form.Control type="text" placeholder="Modalidade" {...register('modalidade')}/>
-                </Form.Group>
-
-
-                <Button variant="primary" type="submit" onClick={handleSubmit(salvar)}>
-                    Salvar
-                </Button>
-            </Form>
+            <Table striped bordered hover className='mb-20'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Passoword</th>
+                        <th>Modalidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data ? data.map(item => (
+                    <tr>
+                        <th>{item.name}</th>
+                        <th>{item.password}</th>
+                        <th>{item.modalidade}</th>
+                    </tr>
+                    )) : ""}
+                </tbody>
+            </Table>
+            <Link href="/form/forms" className='btn btn-success'>Novo</Link>
         </BasePage>
     )
 }
