68a1a2f502c240bfbe9cae6f09af20e9eb2b3ab5
Renato Ostemberg <xrexv158925lpo@gmail.com>
2023-05-24 21:54:11 -0300

    Modifi Addicition

--- Next/learn_next_js/pages/form/[id].js
+++ Next/learn_next_js/pages/form/[id].js
@@ -26,7 +26,7 @@ const id = () => {
 
     function salvar(dados){
         const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
-        cursos.push(dados)
+        cursos.splice(query.id, 1, dados)
         window.localStorage.setItem('cursos', JSON.stringify(cursos))
         push("/form")
     }
