b929d17bdfd94ac18b30a0bcc51aa8fce7dc9833
Renato Ostemberg <xrexv158925lpo@gmail.com>
2023-05-29 18:02:09 -0300

    Firebase Integation

--- Next/learn_next_js/services/firebase.js
+++ Next/learn_next_js/services/firebase.js
@@ -6,15 +6,27 @@ let app
 try {
     app = getApp()
 } catch (error) {
+    // const firebaseConfig = {
+    //     apiKey: process.env.FIREBASE_API_KEY,
+    //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    //     databaseURL: process.env.FIREBASE_DATABASE_URL,
+    //     projectId: process.env.FIREBASE_PROJECT_ID,
+    //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    //     appId: process.env.FIREBASE_APP_ID
+    //   };
+    
     const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
-      };
+        apiKey: "AIzaSyDrYpNPD5_a_I7IqU-TbPWSZL1sNyHFoTg",
+        authDomain: "facultbase.firebaseapp.com",
+        databaseURL: "https://facultbase-default-rtdb.firebaseio.com",
+        projectId: "facultbase",
+        storageBucket: "facultbase.appspot.com",
+        messagingSenderId: "1096619820940",
+        appId: "1:1096619820940:web:035bff037af4b31703f8a9"
+      }; 
+
+    //   console.log(firebaseConfig)
       
       app = initializeApp(firebaseConfig);
 }
